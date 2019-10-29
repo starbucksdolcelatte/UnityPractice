a5362c24ca63bdbc1aabbfd4dba9b46ac35e50b8
starbucksdolcelatte <seoyoon9535@gmail.com>
2019-10-29 15:14:51 +0900

    Player move controller

---
+++ PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform tr;
+    public float moveSpeed = 5.0f;
+
+    // Start is called before the first frame update
+	void Start()
+	{
+        tr = GetComponent<Transform>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        float r = Input.GetAxis("Mouse X");
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        tr.Translate(moveDir.normalized * Time.deltaTime * moveSpeed);
+        tr.Rotate(Vector3.up * r * Time.deltaTime * 60.0f);
+    }
+}
